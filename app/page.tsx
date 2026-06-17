bdb4d5ab0a9e648dc4a72af42eec50f4a6e0779c
pipeotal89 <pipeotal89@gmail.com>
2026-06-17 13:44:25 -0500

    Lil Whimsy

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return(
+    <div>
+      <h1>Te amo mi amor hermosa &lt;3</h1>
+      <img src="https://pbs.twimg.com/media/Ei3yobPUMAExoZu.jpg" /> 
+      <h2>Yo cada que pienso en ti</h2>
+    </div>
+  ) 
 }
